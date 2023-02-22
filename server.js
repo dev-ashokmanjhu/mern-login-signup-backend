277629f1159ec3cbd6489586c8cf24626d10a0f1
dev-ashokmanjhu <dev.ashokmanjhu@gmail.com>
2023-02-22 10:07:17 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const dotenv = require("dotenv");
+mongoose.set("strictQuery", true);
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// database configuration
+mongoose
+  .connect(process.env.DB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+// api routes
+app.get("/", (req, res) => {
+  res.send("Server is Running Succesfully!!!");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = new User({ name, email, password: hashedPassword });
+    await user.save();
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+
+    res.status(201).json({ token });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) throw new Error("Invalid Credentials");
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) throw new Error("Invalid Credentials");
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+
+    res.json({ token });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+app.listen(process.env.PORT || 5000, () =>
+  console.log(`Server started on port ${process.env.PORT || 5000}`)
+);
